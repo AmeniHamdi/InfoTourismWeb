6fcb7193179327b16239f69d52a9d269d31e1381
AmeniHamdi <ameni.hamdi@esprit.tn>
2021-11-20 05:29:39 +0100

    add user auth

---
+++ src/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+
+    // add role
+    // delete user
+    // upload image user
+    //update user
+    // create commande to generate first super_admin user
+}
